bd1f835d33f076b44d076d0c33ae6b4a63dbd1b2
jaishreekori <51723304+jaishreekori@users.noreply.github.com>
2019-07-12 22:17:34 +0530

    Add files via upload

---
+++ Reminder.js
@@ -0,0 +1,178 @@
+import React, { Component } from 'react';
+import { Text, View, StyleSheet, TouchableOpacity, TimePickerAndroid, DatePickerAndroid, TextInput } from 'react-native';
+import AsyncStorage from '@react-native-community/async-storage';
+import { StackActions, NavigationActions } from 'react-navigation';
+
+export default class Reminder extends Component {
+    constructor() {
+        super();
+        this.setDate = this.setDate.bind(this);
+        this.state = {
+            reminderArr: [],
+            chosenDate: new Date(),
+            tasks: [{
+                chosenAndroidTime: '00:00',
+                text: '',
+
+                androidDate: `${new Date().getUTCDate()}/${new Date().getUTCMonth() +
+                    1}/${new Date().getUTCFullYear()}`
+                ,
+            }]
+        };
+    }
+    setDate(newDate) {
+        this.setState({ chosenDate: newDate });
+    }
+
+    setDateAndroid = async () => {
+        try {
+            const { action, year, month, day } = await DatePickerAndroid.open({
+                date: new Date(),
+                minDate: new Date(),
+            });
+            if (action !== DatePickerAndroid.dismissedAction) {
+                this.setState({ androidDate: `${day}/${month + 1}/${year}` });
+            }
+        } catch ({ code, message }) {
+            console.warn('Cannot open date picker', message);
+        }
+    };
+
+    setTimeAndroid = async () => {
+        try {
+            const { action, hour, minute } = await TimePickerAndroid.open({
+                hour: 14,
+                minute: 0,
+                is24Hour: false, // Will display '2 PM'
+            });
+            if (action !== TimePickerAndroid.dismissedAction) {
+                const m = minute < 10 ? `0${minute}` : minute;
+                const h = hour < 10 ? `0${hour}` : hour;
+                console.log(`time: ${hour}:${minute}`);
+                this.setState({ chosenAndroidTime: `${h}:${m}` });
+            }
+        } catch ({ code, message }) {
+            console.warn('Cannot open time picker', message);
+        }
+    };
+
+    changeText = (text) => {
+        this.setState({ text: text })
+    }
+    saveReminder = () => {
+        console.log("this.state.reminderArr " + this.state.reminderArr)
+        this.setState(prevState => ({
+            reminderArr: [...prevState.reminderArr, this.getReminder()]
+        }))
+        //  const tempArr = [...this.state.reminderArr];
+        //  tempArr =this.state.reminderArr;
+        //  tempArr.push(this.getReminder())
+        //  this.setState({reminderArr:tempArr})
+        this.storeData(this.state.reminderArr)
+        this.props.navigation.navigate('ReminderList')
+    }
+    componentDidMount() {
+        this.getData()
+    }
+    getData = async () => {
+        try {
+            const value = await AsyncStorage.getItem('tasks')
+            console.log("value " + [JSON.parse(value)])
+            if (value === undefined || value === null) {
+                this.storeData(this.state.reminderArr)
+            } else {
+                if (value.length > 0) {
+                    this.setState({ reminderArr: value })
+                }
+            }
+        } catch (e) {
+            alert(e)
+        }
+    }
+    getReminder() {
+        return (
+            {
+                androidDate: this.state.androidDate,
+                chosenAndroidTime: this.state.chosenAndroidTime,
+                text: this.state.text,
+            }
+        )
+    }
+    storeData = async (value) => {
+        try {
+            await AsyncStorage.setItem('tasks', JSON.stringify(value))
+            console.log(JSON.stringify(value))
+            // this.props.navigation.navigate('ReminderList');
+        } catch (e) {
+            console.log(e)
+        }
+    }
+    // async saveData() {
+    //     let task = this.getReminder();
+    //     try {
+    //         let localTasks = await AsyncStorage.getItem('tasks');
+    //         if (localTasks == null) {
+    //             let tasks = [];
+    //             this.props.navigation.navigate('ReminderList', { tasks: task })
+    //             console.log('test1',task)
+    //             // tasks.push(task);
+    //             this.storeData(tasks);
+    //         }
+    //         if (localTasks != null) {
+    //             let tasks = JSON.parse(localTasks);
+    //             // this.props.navigation.navigate('ReminderList', { tasks: task })
+    //             tasks.push(task);
+    //             console.log('test2',task)
+    //             this.storeData(tasks);
+    //         }
+    //     } catch (e) {
+    //         alert(e)
+    //     }
+    // }
+    render() {
+        return (
+            <View style={styles.container}>
+                <Text style={styles.textView}>Reminder Date:</Text>
+                <TouchableOpacity
+                    onPress={() => this.setDateAndroid()}
+                    style={{ margin: 10, height: 50, width: "80%", borderWidth: 2, borderColor: 'black', marginBottom: 20, alignSelf: 'center' }}>
+                    <Text style={{ fontSize: 20, fontWeight: 'bold', margin: 10, alignContent: 'center' }}>{this.state.androidDate}</Text>
+                </TouchableOpacity>
+                <Text style={styles.textView}>Reminder Time:</Text>
+                <TouchableOpacity
+                    onPress={() => this.setTimeAndroid()}
+                    style={{ margin: 10, marginBottom: 20, height: 50, width: "80%", borderWidth: 2, borderColor: 'black', alignSelf: 'center' }}>
+                    <Text style={{ fontSize: 20, fontWeight: 'bold', margin: 10, alignContent: 'center' }}>{this.state.chosenAndroidTime}</Text>
+                </TouchableOpacity>
+                <Text style={styles.textView}>Reminder Note:</Text>
+                <TextInput style={{ height: 50, width: "80%", borderWidth: 2, borderColor: 'black', margin: 10, alignSelf: 'center', fontSize: 20 }}
+                    onChangeText={this.changeText}
+                    value={this.state.text}
+                />
+                <TouchableOpacity style={{ height: 40, width: "40%", borderWidth: 2, backgroundColor: 'green', marginTop: 50, alignSelf: 'center' }}
+                    onPress={this.saveReminder}>
+                    <Text style={{ fontSize: 20, fontWeight: 'bold', margin: 10, alignContent: 'center' }}>Save Reminder</Text>
+                </TouchableOpacity>
+                {/* <TouchableOpacity style={{ height: 40, width: "40%", borderWidth: 2, backgroundColor: 'green', marginTop: 50, alignSelf: 'center' }}
+                    onPress={() => {
+                        this.saveData()
+                    }}>
+                    <Text style={{ fontSize: 19, fontWeight: 'bold', margin: 10, alignContent: 'center' }}>Save Reminder</Text>
+                </TouchableOpacity> */}
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        marginTop: 30,
+    },
+    textView: {
+        fontSize: 20,
+        fontWeight: 'bold',
+        marginLeft: 30,
+        color: 'black'
+    }
+});
---
+++ ReminderList.js
@@ -0,0 +1,151 @@
+import React, { Component } from 'react';
+import { View, FlatList, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import AsyncStorage from '@react-native-community/async-storage';
+import { StackActions, NavigationActions } from 'react-navigation';
+
+export default class ReminderList extends Component {
+    constructor() {
+        super();
+        this.state = {
+            tasks: []
+        }
+    }
+    componentDidMount() {
+        this.getData()
+        if(this.props.navigation.state.params === undefined){
+            return;
+        }
+        let task = this.props.navigation.state.params.tasks;
+        let allTasks = this.state.tasks;
+        allTasks.push(task);
+        this.setState({ tasks: allTasks });
+    }
+    getData = async () => {
+        try {
+            const value = await AsyncStorage.getItem('tasks')
+            console.log('GETDATA' ,value)
+            if (value !== null) {
+                this.setState({ tasks: [JSON.parse(value)] })
+            }
+        } catch (e) {
+            alert(e)
+        }
+    }
+    deleteTask = (i) => {
+        this.setState(
+            prevState => {
+                let tasks = prevState.tasks.slice();
+                tasks.splice(i, 1);
+                return { tasks: tasks };
+            },
+        );
+        AsyncStorage.setItem('tasks', JSON.stringify(this.state.tasks))
+    };
+    viewReminder = (data) => {
+        this.props.navigation.navigate('Details', { info: data })
+    }
+    editReminder = (data) => {
+        this.props.navigation.navigate('Reminder', { info: data })
+    }
+    render() {
+        
+            return (
+                <View style={styles.container}>
+                    <Text style={styles.heading}>
+                        Upcoming Reminders
+                    </Text>
+                    <FlatList
+                        style={styles.list}
+                        data={this.state.tasks}
+                        renderItem={({ item, index }) =>
+                            <View style={{ flexDirection: 'row', alignItems: 'center', width: "100%" }}>
+                                <View style={styles.listItemCont}>
+                                    <Text style={styles.listItem}>
+                                        Task:  {item.text}
+                                    </Text>
+                                    <Text style={styles.listItem}>
+                                        Date: {item.androidDate}
+                                    </Text>
+                                    <Text style={styles.listItem}>
+                                        Time: {item.chosenAndroidTime}
+                                    </Text>
+                                </View>
+
+                                <View style={{ flexDirection: 'column' }}>
+                                    <TouchableOpacity style={{ height: 25, width: 50, backgroundColor: 'purple', marginLeft: 30, marginBottom: 7 }}
+                                        onPress={() => this.viewReminder(item)}>
+                                        <Text style={styles.buttonView}>View</Text>
+                                    </TouchableOpacity>
+                                    <TouchableOpacity style={{ height: 25, width: 50, backgroundColor: 'blue', marginLeft: 30, marginBottom: 7 }}
+                                        onPress={() => this.editReminder(item)}>
+                                        <Text style={styles.buttonView}>Edit</Text>
+                                    </TouchableOpacity>
+                                    <TouchableOpacity style={{ height: 25, width: 50, backgroundColor: 'red', marginLeft: 30 }}
+                                        onPress={() => this.deleteTask(index)}>
+                                        <Text style={styles.buttonView}>Delete</Text>
+                                    </TouchableOpacity>
+                                </View>
+                                {/* <TouchableOpacity
+                                        style={{ height: 20, width: 20, marginLeft: 5 }}
+                                        onPress={() => this.checkViewMethod(item, index)}>
+                                        {item.isSelected === true ?
+                                            <View style={{ height: 20, width: 20, backgroundColor: 'blue', marginLeft: 5 }}>
+                                            </View>
+                                            :
+                                            <View style={{ height: 20, width: 20, borderColor: 'black', borderWidth: 3, marginLeft: 5 }}></View>
+                                        }
+                                    </TouchableOpacity> */}
+                                <View style={styles.hr} />
+                            </View>}
+                        extraData={this.state}
+                        keyExtractor={(item, index) => index.toString()}
+                    />
+                </View>
+            );
+
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        marginTop: 10,
+        justifyContent: 'center',
+        alignItems: 'flex-start',
+    },
+    buttonView: {
+        fontSize: 15,
+        color: 'white',
+        alignSelf: 'center'
+    },
+    heading: {
+        color: 'black',
+        fontFamily: 'Helvetica',
+        justifyContent: 'center',
+        alignItems: 'center',
+        alignSelf: 'center',
+        fontSize: 30,
+        fontWeight: 'bold',
+    },
+    list: {
+        marginTop: 30,
+        marginLeft: 20,
+        width: "100%",
+    },
+    listItem: {
+        margin: 5,
+        fontSize: 18,
+        color: 'black'
+    },
+    hr: {
+        height: 1,
+        backgroundColor: 'black'
+    },
+    listItemCont: {
+        width: "70%",
+        flexDirection: "column",
+        borderWidth: 2,
+        marginBottom: 20,
+        justifyContent: "space-between"
+    },
+})
